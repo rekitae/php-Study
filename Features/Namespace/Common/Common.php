079b1c62afa0d8558cdeefc61c18b91068ef72b7
gitagy <gitagy@gmail.com>
2017-01-10 01:44:44 +0900

    add

--- Features/Namespace/Common/Common.php
+++ Features/Namespace/Common/Common.php
@@ -3,8 +3,48 @@ namespace rkt\Common;
 
 class F
 {
+    private $cached;
+
+    public static $instance;
     public static $s;
     public static $ss;
+
+    function __construct()
+    {
+        $this->cached = new \stdClass;
+    }
+
+/*
+    function __get(string $name)
+    {
+        if ($this->{$name})
+        {
+            return;
+        }
+
+        $func = function() { };
+
+        if (function_exists($name))
+        {
+            $func = function()
+            {
+                return function()
+                call_user_func($name, func_get_args());
+            }
+
+        }
+
+        return $func;
+    }
+*/
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance))
+            self::$instance = new self;
+
+        return self::$instance;
+    }
 
     public static function init()
     {
@@ -41,4 +81,14 @@ class F
     {
         return $a . $b;
     }
+
+    public function sumInstance($a, $b)
+    {
+        return $a+$b;
+    }
+}
+
+function map($callable, $list)
+{
+    return array_map($callable, $list);
 }
--- Features/Namespace/test.php
+++ Features/Namespace/test.php
@@ -1,6 +1,7 @@
 <?php
 require_once('bootstrap.php');
 
+use rkt\Common;
 use rkt\Common\F as F;
 
 F::init();
@@ -24,16 +25,28 @@ $list = [1,2,3];
 
 $res = (object)[];
 
+//$a = [sumFunc(), sumFunc()];
 //$list.map(x => x+1).reduce()
 
 $res->a1 = array_map($func, $list);
-//$res->a2 = Common\map($func, $list);
+$res->a2 = Common\map($func, $list);
 
 $res->b1 = array_reduce($list, function($a, $b) { return F::sum($a, $b); }, 0);
 $res->b2 = array_reduce($list, $sum, 0);
+
 $res->b3 = array_reduce($list, F::$s,  0);
 $res->b4 = array_reduce($list, F::$ss,  0);
-$res->b5 = array_reduce($list, 'sumFunc',  0);
+
+$res->b5 = array_reduce($list, sumFunc,  0);
+$res->b6 = array_reduce($list, 'sumFunc',  0);
+
+//$res->b7 = array_reduce($list, F::getInstance(),  0);
+
+$str = "i'm a boy";
+$temp = array_map('trim', explode(' ', $str));
+print_r($temp);
+
+exit;
 
 //$res->b2 = Common\reduce('sum', $list, 0);
 //$res->b3 = Common\reduce('sum', $list, 1);
